4e2a506738340365d66326021039b112bf4ffc0a
ChanMi Jung <prinpice@naver.com>
2021-11-21 11:02:43 +0900

    Update dir_path in kss.py

--- preprocess/kss.py
+++ preprocess/kss.py
@@ -1,18 +1,21 @@
 import os
 import kss
 
-file_list = os.listdir('./data')
+input_dir = './data'
+output_dir = './outputs'
+
+file_list = os.listdir(input_dir)
 
 text = ''
 # 정제된 데이터를 kss 라이브러리를 사용하여 문장분리함
 for name in file_list:
-    with open(f'./data/{name}', 'r', encoding='UTF-8') as f:
+    with open(f'{input_dir}/{name}', 'r', encoding='UTF-8') as f:
         text += f.read()
 sentences = kss.split_sentences(text)
     
 # 분리된 문장들을 각각 txt로 저장
 index = 1
 while index <= len(sentences):
-    with open(f'./outputs/{index}.txt', 'w') as file:
+    with open(f'{output_dir}/{index}.txt', 'w') as file:
         file.writelines(sentences[index-1])
     index += 1
